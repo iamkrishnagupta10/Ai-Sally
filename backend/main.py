9ffb7333a11089820d90265970b8de8465dc2296
Krishna Gupta <129900110+iamkrishnagupta10@users.noreply.github.com>
2023-09-29 16:31:44 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ origins = [
 # CORS - Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
